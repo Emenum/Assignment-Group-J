938de56f09f051e14c4c0309b8efe2ea5aed463e
Chinwendu Emenike <echinwenduvictoriae@gmail.com>
2023-10-15 21:08:04 +0100

    final group work

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 ## An assignment project for DevCareer Africa tech programme
+Copiled by:
+@Jeremiah
+
+@Chynwe
+
+@lilmisssomeone
+
+@Emenum Vera
+
+@Mustapha Sodiq Abubakar
+
+@Felix.E.O
+
+@Cephas Onwuliri
+
+@shedy
+
+@Oluwademilade
+
+@victorbrown
--- index.js
+++ index.js
@@ -1,65 +1,84 @@
-const gsm = document.getElementById("phoneNumber")
-const code = document.getElementById("countryCode")
+const numberInput = document.querySelector('#phoneNumber')
+// const code = document.getElementById("countryCode")
 let regExp1 = /^(\d{3})$/
 //    (234\s?)
+
+validatenetwork();
 
 function validatephonenumber (str){
     const regExp2 = /^(\d{3})(\d{3})?(\d{4})$/;
     return regExp2.test(str);
 }
-let phonevalue = gsm.value
 
-function validatephone(e){
-    if((!validatephonenumber(phonevalue))){
-        console.log("Wrong phone number format")
-    } else{
-        console.log("correct value")
-    }
-    e.preventDefault();
-}
+function validatenetwork(){
 
-function validatecountry(){
-
-    let toArray = phonevalue.split("")
-    let splicetoArray = toArray.splice(0,3)
-    let arraytostring = splicetoArray.join()
-    phonevalue.addEventListener("keydown",()=>{
-        if(arraytostring === "813"){
-        document.querySelector(".telco_logos,.telco1").classList.remove("hidden");
-        }else if(arraytostring === "812"){
-            document.querySelector(".telco2").classList.remove("hidden");
-        }else if(arraytostring === "815"){
-            document.querySelector(".telco3").classList.remove("hidden");
-        }else if(arraytostring === "819"){
-            document.querySelector(".telco4").classList.remove("hidden");
-        };
-        
+    numberInput.addEventListener('input',(event)=>{
+        const inputvalue = event.target.value
+        if(inputvalue.length >= 3){
+            const fistThreeDigits = inputvalue.slice(0,3);
+            switch(fistThreeDigits){
+                case "813":
+                case "803":
+                case "806":
+                case "903":
+                case "906":
+                case "702":
+                case "706":
+                case "703":
+                case "816":
+                 document.querySelector(".mtn").classList.remove("hidden");
+                 document.querySelector(".airtel").classList.add("hidden");
+                 document.querySelector(".glo").classList.add("hidden");
+                 document.querySelector(".etisalat").classList.add("hidden");
+            break;
+                case "812":
+                case "901":
+                case "902":
+                case "904":
+                case "907":
+                case "802":
+                case "808":
+                case "708":
+                case "701":
+                document.querySelector(".airtel").classList.remove("hidden");
+                document.querySelector(".mtn").classList.add("hidden");
+                 document.querySelector(".glo").classList.add("hidden");
+                 document.querySelector(".etisalat").classList.add("hidden");
+            break;
+            case "815":
+            case "905":
+            case "805":
+            case "807":
+            case "705":
+                document.querySelector(".glo").classList.remove("hidden");
+                document.querySelector(".airtel").classList.add("hidden");
+                 document.querySelector(".mtn").classList.add("hidden");
+                 document.querySelector(".etisalat").classList.add("hidden");
+            break;
+            case "819":
+            case "908":
+            case "809":
+                document.querySelector(".etisalat").classList.remove("hidden");
+                document.querySelector(".airtel").classList.add("hidden");
+                document.querySelector(".glo").classList.add("hidden");
+                document.querySelector(".mtn").classList.add("hidden");
+            break;
+            default: console.log("other networks");
+            }
+        } else{
+            console.log("other networks")
+        }
+        console.log(fistThreeDigits)
     })
+
+    
+   
 }
 
-document.getElementById("my_form").addEventListener("click", validatecountry);
-
-let g = "swing"
-let h = g.split("")
-console.log(h.splice(0,3))
-// code.addEventListener("input",(e)=>{
-//     if (gsm.value == regExp2){
-//     console.log ("Correct phone format") 
-// } else{
-//     console.log("Wrong phone format")
-// }
-// })
-
-console.log(gsm.value)
+document.getElementById("my_form").addEventListener("submit", validatephonenumber);
 
 
 
-// function validatephone(str){
-//     return regExp2.test(str)
-// }
-// console.log (validatephone("8130305905"))
 
-// if (regExp.test()=== true){
-//     document.getElementsByTagName("input").innerText="Phone number in the wrong format"
-//     con
-// }
+
+
